c5b26667e0ce7631e328767196c1c4952ee47c4f
Asadbek <huntsman6004@gmail.com>
2024-05-10 22:11:00 +0500

    update: production settings

--- core/settings/production.py
+++ core/settings/production.py
@@ -4,24 +4,20 @@ from .base import *  # noqa
 # General
 ###################################################################
 
-DEBUG = True
+DEBUG = False
 
 ###################################################################
 # Django security
 ###################################################################
 
-# USE_X_FORWARDED_HOST = True
-# SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
+USE_X_FORWARDED_HOST = True
+SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
 
 CSRF_COOKIE_SECURE = True
 CSRF_TRUSTED_ORIGINS = [
     "http://localhost",
     "http://127.0.0.1",
-    "http://127.0.0.1:8000",
-    "http://heatwarehouse.pythonanywhere.com",
-    "https://heatwarehouse.pythonanywhere.com",
-    "http://heat-warehouse.xn--0h8h.uz",
-    "https://heat-warehouse.xn--0h8h.uz",
+    "https://example.com",
 ]
 
 ###################################################################
