b5da82237520716ec4e159bb68cdfe872fa21139
Asadbek <huntsman6004@gmail.com>
2024-05-26 23:48:42 +0500

    update: production settings

--- core/settings/production.py
+++ core/settings/production.py
@@ -24,6 +24,6 @@ CSRF_TRUSTED_ORIGINS = [
 # CORS
 ###################################################################
 
-CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOW_HEADERS = ["*"]
